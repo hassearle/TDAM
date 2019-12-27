7a9051ec73bddd7e8bba1ede4c58d081002e104f
has <a.searle@gmail.com>
2019-12-27 08:05:18 -0800

    setup()/tearDown()

--- gCodeHashTest.py
+++ gCodeHashTest.py
@@ -7,14 +7,34 @@
 '''
 
 import unittest
-import gCodeHash
+import gGodeHash
+from os import remove as rm
 
-class GCodeHashTest(unittest.TestCase):
+
+class GCodeHashTestNominalSetup(unittest.TestCase):
 	def setup():
 		pass
 		
-	def test100_900_hashFileMissing(self):
-		expectedResult = "hello world"
+	# def test100_500_hashFileMissing(self):
+	# 	expectedResult = "output: ERROR: hash file misssing"
+	# 	actualResult = gCodeHash.gch()
+	# 	self.assertEqual(expectedResult, actualResult)
+
+class GCodeHashTestMissingHash(unittest.TestCase):
+	def setup(self):
+		fSTL =  open("sTLTemp", "w+")
+		# fHash = open("hashTemp", "w+")
+		fV3dpConfig = open("v3dpConfigTemp", "w+")
+		fGCTests =  open("gCTestsTemp", "w+")
+
+	def tearDown(self):
+		rm("sTLTemp")
+		# rm("hashTemp")
+		rm("v3dpConfigTemp")
+		rm("gCTestsTemp")
+
+	def test100_500_hashFileMissing(self):
+		expectedResult = "output: ERROR: hash file misssing"
 		actualResult = gCodeHash.gch()
 		self.assertEqual(expectedResult, actualResult)
 
