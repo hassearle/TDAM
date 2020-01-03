143daa08672aa29a8779d497c2383d4dde96e0dd
has <a.searle@gmail.com>
2020-01-03 07:21:52 -0800

    init unitTests

---
+++ unitTests.py
@@ -0,0 +1,10 @@
+'''
+    author:    Ash Searle    kss0024@auburn.edu
+    created:   1/3/20
+    updated:   1/3/20
+    
+    purpose:   gCodeHash unit tests
+'''
+
+import unittest
+from gCodeHash import gch
