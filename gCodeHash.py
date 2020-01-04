c14a53c962c50a38edfd6e9a00ca7a548280fcff
has <a.searle@gmail.com>
2020-01-04 06:25:09 -0800

    test300_200_validateParms: test100_150_NominalSTLHashPassesGCodeTestCases

--- gCodeHash.py
+++ gCodeHash.py
@@ -7,73 +7,77 @@
 '''
 
 ERROR_HEADER = "ERROR: "
-OUTPUT_HEADER = "output: "
+STATUS_HEADER = "status: "
+STATUS_POSITIVE = "ok"
 ERROR01 = "hash file missing"
 ERROR02 = "STL file missing"
 ERROR03 = "V3DPConfig file missing"
 ERROR04 = "GCTests file missing"
 
 class V3dpos:
-	stl = "n/a"
+	objStatus = "n/a"
+	objStl = "n/a"
 	objHash = "n/a"
 	objV3dpConfig = "n/a"
 	objGCTests = "n/a"
-	def __init__ (self, stl_, hash_, v3dpConfig_, gCTests_):
-		self.stl = stl_
+	def __init__ (self, status_, stl_, hash_, v3dpConfig_, gCTests_):
+		self.objStatus = status_
+		self.objStl = stl_
 		self.objHash = hash_
 		self.objV3dpConfig = v3dpConfig_
 		self.objGCTests = gCTests_
 
 def gch(flag):
-	result = 1
-
-	try:
-		try:
-			fHash = open("hash", "r")
-		except:
-			raise ValueError(ERROR01)
-		try:
-			fSTL = open("stl", "r")
-		except:
-			raise ValueError(ERROR02)
-		try:
-			fV3DPConfig = open("v3DPConfig", "r")
-		except:
-			raise ValueError(ERROR03)
-		try:
-			fGCTests = open("gcTests", "r")
-		except:
-			raise ValueError(ERROR04)
-	except Exception as e:
-		result = OUTPUT_HEADER + ERROR_HEADER + e.args[0]
-
-	stlInput = ""
-	with open('inputFiles/stl.stl', 'r') as f:
-		stlInput += f.read()
-
-	hashInput = ""
-	with open('inputFiles/hash', 'r') as f:
-		hashInput += f.read()
-
-	v3dpConfigInput = ""
-	with open('config/v3dpConfig', 'r') as f:
-		v3dpConfigInput += f.read()
-
-	gCTestsInput = ""
-	with open('config/gCTests.py', 'r') as f:
-		gCTestsInput += f.read()
-
-	v3dpos = V3dpos(stlInput,hashInput,v3dpConfigInput,gCTestsInput)
-
 	if flag != 0:
-		rtn = {"status":result,
-			"stl":v3dpos.stl,
+		rtn = {"output":result,
+			"stl":v3dpos.objStl,
 			"hash":v3dpos.objHash, 
 			"v3dpConfig":v3dpos.objV3dpConfig,
 			"gCTests":v3dpos.objGCTests}
 		return rtn
 	else:
 		return result
+
+def validateParms(stl_, hash_, v3dpConfig_, gCTests_):
+	try:
+		try:
+			hashInput = ""
+			with open('inputFiles/hash', 'r') as f:
+				hashInput += f.read()
+		except:
+			raise ValueError(ERROR01)
+		try:
+			stlInput = ""
+			with open('inputFiles/stl.stl', 'r') as f:
+				stlInput += f.read()
+		except:
+			raise ValueError(ERROR02)
+		try:
+			v3dpConfigInput = ""
+			with open('config/v3dpConfig', 'r') as f:
+				v3dpConfigInput += f.read()
+		except:
+			raise ValueError(ERROR03)
+		try:
+			gCTestsInput = ""
+			with open('config/gCTests.py', 'r') as f:
+				gCTestsInput += f.read()
+		except:
+			raise ValueError(ERROR04)
+	except Exception as e:
+		result = ERROR_HEADER + e.args[0]
+		return result
+	else:
+		status = STATUS_POSITIVE
+
+	rtn = V3dpos(status,stlInput,hashInput,v3dpConfigInput,gCTestsInput)
+	result = {"status":rtn.objStatus,
+			"stl":rtn.objStl,
+			"hash":rtn.objHash, 
+			"v3dpConfig":rtn.objV3dpConfig,
+			"gCTests":rtn.objGCTests}
+
+	return result
 
 # stlInput = ""
 # with open('inputFiles/stl.stl', 'r') as f:
