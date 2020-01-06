9b75746c297bc6a9538db344b947c27c8e3459ac
has <a.searle@gmail.com>
2020-01-05 21:44:58 -0800

    test200_500_FailsGCTests

--- unitTests.py
+++ unitTests.py
@@ -11,16 +11,20 @@ from os import remove as rm
 import gCodeHash
 from gCodeHash import V3dpos
 from hashlib import sha512
+from pprint import pprint
 
 #100: V3dpos
 #200: GCodeTests
 #300: validateParams
+#400: sliceSTLToGCode
 
 # 1##:	happy path test case
 # 1#1:	similarly grouped test case
 # 2##:	refactor (blue light) test case
 # 5##:	dev (temp) test case
 # 9##:	sad path test case
+
+GCODE_PATH = 'stlFiles/testGCode.gcode'
 
 class GCodeHashTestNominalSetup(unittest.TestCase):
 
@@ -88,36 +92,52 @@ class GCodeHashTestNominalSetup(unittest.TestCase):
 		actualResult = result["gCTests"]
 		self.assertEqual(expectedResult, actualResult)
 
-	# def test200_931_FailsGCTests(self):
-	# 	expectedResult = "output: ERROR: sliced STL GCode failed GCTests"
-	# 	gcode = 
-	# 	actualResult = gCTests()
+	def test200_900_FailsGCTests(self):
+		expectedResult ='ERROR: sliced STL GCode failed GCTests'
+		gcode = ""
+		with open(GCODE_PATH, 'r') as f:
+			gcode = f.read()
+		obj = gCodeHash.validateParms()
+		obj.objGCode = gcode
+		result = gCodeHash.validateGCode(obj)
+		# pprint(vars(obj))
+		actualResult = result.objStatus
+		self.assertEqual(expectedResult, actualResult)
 
 	def test300_200_validateParms(self):
 		stl_ = ""
-		with open('stlFiles/testSTL.stl', 'r') as f:
-			stl_ += f.read()
+		with open('stlFiles/testSTL.stl', 'rb') as f:
+			stl_ = f.read()
 
 		hash_ = "e7c1d1d8c8d8f377889b8ecad6a5f5f0e4e98362980a8905b708f10ae84f6a91d607377713e4ed0d7bcfee4dc3a1242c7a3e4dc81c9f3bc1c27a548dfd7b6b12  stl.stl"
 		
 		v3dpConfig_ = ""
 		with open('stlFiles/testV3dpConfig', 'r') as f:
-			v3dpConfig_ += f.read()
+			v3dpConfig_ = f.read()
 
 		gcTests_ = ""
-		with open('stlFiles/testGCTests.py', 'r') as f:
-			gcTests_ += f.read()
+		with open('config/gCTests.py', 'r') as f:
+			gcTests_ = f.read()
 
 		expectedResult = {"status":'ok',
 			"stl":stl_,
 			"hash":hash_, 
 			"v3dpConfig":v3dpConfig_,
 			"gCTests":gcTests_}
-		actualResult = gCodeHash.validateParms(stl_,hash_,v3dpConfig_,gcTests_)
+		result = gCodeHash.validateParms()
+		actualResult = {"status":result.objStatus,
+			"stl":result.objStl,
+			"hash":result.objHash, 
+			"v3dpConfig":result.objV3dpConfig,
+			"gCTests":result.objGCTests}
 		self.assertEqual(expectedResult, actualResult)
 
+	def test400_900_gCodeFileMissingAfterSlicing(self):
+		pass
 
-#class method to slice gcode
+	# sliceSTLToGCode
+
+	#class method to slice gcode
 
 		# h = sha512()
 		# h.update(stl)
